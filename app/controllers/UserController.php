b25771229b04a77a0aad2a2605eb22174c6f8376
Ankares <vasya-petrenko-99@mail.ru>
2022-10-06 10:44:22 +0300

    corrections

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -1,13 +1,15 @@
 <?php
 
 namespace App\Controllers;
+use App\Models;
 use App\Core\Controller;
+use App\Core\ServiceProvider;
+use App\Models\Repositories\IUserProcessing;
 use App\Models\Repositories\UserRepository;
 use App\Models\UserModel;
 
 class UserController extends Controller
 {
-    private $data = [];
 
     public function __construct(
         private UserModel $user,
@@ -16,41 +18,44 @@ class UserController extends Controller
 
     public function index() 
     {
+        $data = null;
         if (isset($_POST['email'])) {
             $this->user->setData($_POST);
             $this->repository->checkUser($this->user, $_POST['email']);
             $this->user->validation();
             if (empty($this->user->error)) {
                 $this->repository->add($this->user);
-                $this->data['success'] = 'User successfully added';
+                $data['success'] = 'User successfully added';
             } else {
-                $this->data['error'] = $this->user->error;
+                $data['error'] = $this->user->error;
             }
         }
-        $this->view('user/index', $this->data);
+        $this->view('user/index', $data);
     }
 
     public function list()
     {
-        $this->data['users'] = $this->repository->getAllData();
-        $this->view('user/list', $this->data);
+        $data = null;
+        $data['users'] = $this->repository->getAllData();
+        $this->view('user/list', $data);
     }
 
     public function edit($id)
     {
+        $data = null;
         if (isset($_POST['id'])) {
             $this->user->setData($_POST);
             $this->repository->checkUser($this->user, $_POST['email'], $_POST['id']);
             $this->user->validation();
             if (empty($this->user->error)) {
                 $this->repository->updateUser($this->user, $_POST['id']);   
-                $this->data['success'] = 'Successfully updated';
+                $data['success'] = 'Successfully updated';
             } else {
-                $this->data['error'] = $this->user->error;
+                $data['error'] = $this->user->error;
             }            
         }
-        $this->data['user'] = $this->repository->getDataByID($id);
-        $this->view('user/edit', $this->data);
+        $data['user'] = $this->repository->getDataByID($id);
+        $this->view('user/edit', $data);
     }
 
     public function delete()
--- app/database/migration.php
+++ app/database/migration.php
@@ -1,12 +1,12 @@
 <?php
+
 use App\Models\DB;
 
 require 'vendor/autoload.php';
 
 (Dotenv\Dotenv::createImmutable(__DIR__ . '\..\..'))->load();
 
-
-$db = DB::getInstance('localhost');
+$db = DB::getInstance();
 $files = getMigrationFiles($db);
 
 if (empty($files)) {
